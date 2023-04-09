1590a368463189d2f028549e34966ad779e7ad01
tolgahanbora <tolgahanbora@outlook.com>
2023-04-10 02:25:34 +0300

    finish

--- server/routes/property.routes.js
+++ server/routes/property.routes.js
@@ -6,9 +6,9 @@ const router = express.Router()
 
 
 router.route('/').get(getAllProperties)
-router.route('/id').get(getPropertyDetail)
+router.route('/:id').get(getPropertyDetail)
 router.route('/').post(createProperty)
-router.route('/id').patch(updateProperty)
-router.route('/id').delete(deleteProperty)
+router.route('/:id').patch(updateProperty)
+router.route('/:id').delete(deleteProperty)
 
 export default router
